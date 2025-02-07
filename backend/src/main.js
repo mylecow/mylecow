30c8bbe5490f8926669df14c86dc0521d331f248
Cesar Augusto Arana Collazos <ksrarc@gmail.com>
2025-02-06 20:19:14 -0500

    PR comments

--- backend/src/main.js
+++ backend/src/main.js
@@ -1,5 +1,9 @@
 const express = require('express'); 
-const app = express(); 
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send('muuuu');
+})
 
 app.listen(3000, ()=>{
     console.log('Server is running on port 3000')
