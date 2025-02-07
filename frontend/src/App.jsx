4ab5680976d6e7e64329544962fdf41d082e484a
Cesar Augusto Arana Collazos <ksrarc@gmail.com>
2025-02-06 20:58:27 -0500

    Added sample styled component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,25 @@
+import styled from 'styled-components'
 import './App.css'
+import { Centered } from './components/layout'
+
+const MyCentered = styled(Centered)`
+  gap: 1rem;
+  flex-direction: column;
+
+  & * {
+    background-color: gray;
+  }
+`;
 
 function App() {
 
   return (
     <>
       <h1>My Lecow</h1>
+      <MyCentered style={{backgroundColor: 'lightblue'}}>
+        <h2>Prueba</h2>
+        <h2>Rabbit</h2>
+      </MyCentered>
     </>
   )
 }
---
+++ frontend/src/components/layout.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+/// Put here generics and util components
+
+export const Centered = styled.div`
+    display: flex;
+    flex=direction: row;
+    align-items: center;
+    justify-content: center;
+`;
